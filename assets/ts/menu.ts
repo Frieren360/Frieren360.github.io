f880d5317fe5d7c6fa4321dd19a353cc5d9235d5
Frieren360 <>
2026-04-20 23:11:58 +0100

    debloat hamburger menu lol

---
+++ assets/ts/menu.ts
@@ -0,0 +1,9 @@
+/**
+ * DEBLOATED!!
+ * Slide up/down
+ * Code from https://dev.to/bmsvieira/vanilla-js-slidedown-up-4dkn
+ * @param target 
+ * @param duration 
+ */
+export default function () {
+}
